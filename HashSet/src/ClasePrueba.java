fbf834704795ae0e8612f035222382c0f0adcdef
Gabriel-ghub <romerogabriel.ad@gmail.com>
2022-01-12 18:36:11 +0100

    Con cambios correctos

--- HashSet/src/ClasePrueba.java
+++ HashSet/src/ClasePrueba.java
@@ -1,5 +1,5 @@
 public class ClasePrueba {
     public static void main(String[] args) {
-
+        System.out.println("Esta prueba es para ver si es posible realizar un commit desde una carpeta que no sea la principal");
     }
 }

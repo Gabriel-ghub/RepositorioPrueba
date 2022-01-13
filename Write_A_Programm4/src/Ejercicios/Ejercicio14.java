12b4cc12b09df242bf6d0dd824db43783603980b
Gabriel-hub <romerogabriel.ad@gmail.com>
2022-01-13 19:23:49 +0100

    Intento de ejercicio 14

--- Write_A_Programm4/src/Ejercicios/Ejercicio14.java
+++ Write_A_Programm4/src/Ejercicios/Ejercicio14.java
@@ -4,24 +4,28 @@ import java.util.Calendar;
 import java.util.HashMap;
 
 public class Ejercicio14 {
-  /*  public HashMap<String, Calendar> diary;
+  public HashMap<String, Calendar> diary;
 
     public Ejercicio14(){
-        diary = new HashMap<String, Calendar>();
+        this.diary = new HashMap<String,Calendar>();
     }
-    public void addEjercicio14(String name, int day, int month, int year){
-
-    }
-    public Calendar getEjercicio14for(String name){
-
-
-    }
-    public void printEjercicio14(){
-
-    }
-*/
-    public static void main(String[] args) {
+    public void addBirthday(String name, int day, int month, int year){
         Calendar cal = Calendar.getInstance();
-        System.out.println(cal.get(Calendar.YEAR));
+        cal.set(year, month, day);
+        diary.put(name, cal);
+    }
+    public Calendar getBirthdayFor(String name){
+        return diary.get(name);
+    }
+    public void printDiary(){
+        System.out.println(diary);
+    }
+
+    public static void main(String[] args) {
+        Ejercicio14 d1 = new Ejercicio14();
+        d1.addBirthday("Laura", 12,12,12);
+        d1.addBirthday("Marcos", 13, 5,24);
+        //d1.printDiary();
+        System.out.println(d1.getBirthdayFor("Laura"));
     }
 }
